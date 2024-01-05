82bf6cc1e73aceeab31b534f7bc0198cd683cfeb
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-05 11:07:52 +0530

    update: css styling wrong

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,28 @@ import { useState } from "react";
 
 const App = () => {
   const [state, setState] = useState(Array(9).fill(null));
+  
   console.log(state);
 
   return (
-    <div className="h-screen w-full p-20">
-
+    <div className="h-screen w-full pt-40 relative">
+      {/* p-1/2 translate-x-1/2 */}
       <div className="flex row">
-        <Block value="X" />
-        <Block value="X" />
-        <Block value="X" />
+        <Block value={state[0]}  />
+        <Block value={state[1]}  />
+        <Block value={state[2]}  />
       </div>
       
       <div className=" flex row">
-        <Block value="X" />
-        <Block value="X" />
-        <Block value="X" />
+        <Block value={state[3]}  />
+        <Block value={state[4]}  />
+        <Block value={state[5]}  />
       </div>
       
       <div className=" flex row">
-        <Block value="X" />
-        <Block value="X" />
-        <Block value="X" />
+        <Block value={state[6]}  />
+        <Block value={state[7]}  />
+        <Block value={state[8]}  />
       </div>
     
       </div>
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,11 +1,11 @@
 
 interface BlockProps {
-    value: string | null;
+    value?: string | null;
 }
 
 const Block: React.FC<BlockProps> = (props) => {
     return (
-        <div className=" border border-black h-40 w-40 flex items-center justify-center cursor-pointer font-semibold text-5xl">
+        <div className=" border border-black h-40 w-40 flex items-center justify-center cursor-pointer font-semibold text-5xl left-1/2">
             {props.value}
         </div>
     )
